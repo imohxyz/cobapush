88446635eda56beb56db0ab4c1a867ed2702b314
masimoh98 <masimoh98@gmail.com>
2019-05-08 15:13:43 +0700

    perubahan

--- pushgit.bat
+++ pushgit.bat
@@ -3,13 +3,13 @@ SET day=%DATE:~7,2%
 SET year=%DATE:~10%
 
 
-> ECHO cd D:/
-> ECHO cd BitKit
-> ECHO cd Tabulasi2
-> ECHO git add .
-> ECHO git commit -m "perubahan"
-> ECHO git push origin master
-> ECHO Haqiqi@2019
+cd D:/
+cd BitKit
+cd Tabulasi2
+git add .
+git commit -m "perubahan"
+git push origin master
+Haqiqi@2019
 
-> ECHO bye
+bye
 
