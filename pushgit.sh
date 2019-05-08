d8b047637f1cefca4d4084e4de2364c5f11df337
masimoh98 <masimoh98@gmail.com>
2019-05-08 14:01:04 +0700

    perubahan terbaru

--- pushgit.sh
+++ pushgit.sh
@@ -2,7 +2,7 @@
 ###########################
 cd D:/BitKit/Tabulasi-2
 # switch to branch you want to use
-git checkout master
+#git checkout master
 # add all added/modified files
 git add .
 # commit changes
@@ -10,6 +10,7 @@ git add .
 git commit -am "perubahan terbaru"
 # push to git remote repository
 git push origin master
+echo Haqiqi@2019
 ###########################
 echo Press Enter...
 read
