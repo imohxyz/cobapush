7ab4af101e46636059daf3b2b33e7bfcfcfbe724
masimoh98 <masimoh98@gmail.com>
2019-05-08 14:16:17 +0700

    perubahan terbaru

--- pushgit.sh
+++ pushgit.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
+$pass=Haqiqi@2019
 ###########################
 cd D:/BitKit/Tabulasi-2
 # switch to branch you want to use
 #git checkout master
 # add all added/modified files
 git add .
+# exec echo "$GIT_PASSWORD"
 # commit changes
 #read commitMessage
 git commit -am "perubahan terbaru"
 # push to git remote repository
 git push origin master
-exec D:/BitKit/Tabulasi-2/gitpassword.sh
+read $pass
 ###########################
 echo Press Enter...
 read
